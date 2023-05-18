258738f935686b0df02999d8c21c085cfa8a3b93
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-18 16:31:24 +0200

    add pseudo soluction for the limitation of 4 num

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,2 +1,6 @@
-
+const numeroquattrocifre = parseInt(prompt("Scrivi un numero di massimo 4 cifre"));
+console.log(numeroquattrocifre)
+if (numeroquattrocifre >= 0 && numeroquattrocifre < 9999){
+    
+}
 
