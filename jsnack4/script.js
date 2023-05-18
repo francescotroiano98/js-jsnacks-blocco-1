4691bf454dbac9295cf633b445b0251b01a53a2f
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-18 16:56:55 +0200

    Pseudo soluction

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,6 +1,8 @@
 const numeroquattrocifre = parseInt(prompt("Scrivi un numero di massimo 4 cifre"));
 console.log(numeroquattrocifre)
 if (numeroquattrocifre >= 0 && numeroquattrocifre < 9999){
-    
+
+} else{
+    alert("Non hai inserito un valore valido, massimo 4 cifre");
 }
 
