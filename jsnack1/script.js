7704855ef262ac80ee86cb51107fb9d1d19bcc3b
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-18 15:45:17 +0200

    Done 1° Snack

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,7 @@
-
-
+let sommaNumeri = 0
+for(let i = 0; i < 3; i++){
+ 
+    let number = parseInt( prompt ("Scrivi un numero"));
+    sommaNumeri = sommaNumeri + number;
+    document.getElementById("answer").innerHTML = sommaNumeri;
+}
