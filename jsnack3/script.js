1263f09cc91a93709c48e0c0ab2160adc95ae2a1
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-18 16:10:32 +0200

    Done 3° snack

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,2 +1,18 @@
+let NumeriDispari = [];
+let InsertNumber = 0;
 
+for (i=0; i<6; i++){
 
+    let number = parseInt( prompt("Scrivi un numero"));
+    if(number % 2 === 0){
+        
+    } else {
+
+    InsertNumber = NumeriDispari.push(number);
+
+    }
+
+}
+
+document.getElementById("dispari").innerHTML = NumeriDispari;
+console.log(NumeriDispari);
