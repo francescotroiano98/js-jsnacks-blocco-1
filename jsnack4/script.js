10bbb9941294be371521be84fe1147d1b91027c3
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-19 10:55:52 +0200

    Done 4° snack

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,8 +1,15 @@
 const numeroquattrocifre = parseInt(prompt("Scrivi un numero di massimo 4 cifre"));
 console.log(numeroquattrocifre)
-if (numeroquattrocifre >= 0 && numeroquattrocifre < 9999){
+if (numeroquattrocifre > 999 && numeroquattrocifre <= 9999){
+    const array = Array.from(String(numeroquattrocifre), Number);
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+        console.log(sum)
+    }
 
+    document.getElementById("answer").innerHTML = sum;
+    console.log(sum);
 } else{
     alert("Non hai inserito un valore valido, massimo 4 cifre");
 }
-
